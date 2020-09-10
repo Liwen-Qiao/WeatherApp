172bc045df30f06ad641b1b9de98ff0ca6680e85
Liwen Qiao <lianna.qiao.lw@gmail.com>
2020-09-10 00:28:48 -0400

    setup MVVM

---
+++ WeatherApp/WeatherApp/ViewController/WALocationListMainView.swift
@@ -0,0 +1,20 @@
+//
+//  WALocationListMainView.swift
+//  WeatherApp
+//
+//  Created by qiaoliwen on 2020/9/10.
+//  Copyright © 2020 qiaoliwen. All rights reserved.
+//
+
+import UIKit
+import EasyPeasy
+
+class WALocationListMainView: UIViewController {
+    
+    var coreDataStack: WACoreDataStack!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "WeFunGame"
+    }
+}
---
+++ WeatherApp/WeatherApp/WAConstant.swift
@@ -0,0 +1,9 @@
+//
+//  WAConstant.swift
+//  WeatherApp
+//
+//  Created by qiaoliwen on 2020/9/10.
+//  Copyright © 2020 qiaoliwen. All rights reserved.
+//
+
+import Foundation
